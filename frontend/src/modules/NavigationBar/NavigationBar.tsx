3f3ae35b6672b0518596d90f4f36ddee9851e2c8
CristhianAc <cristhian_agamez@outook.com>
2024-10-05 16:26:46 -0500

    Refactor code to add NavigationBar component

---
+++ frontend/src/modules/NavigationBar/NavigationBar.tsx
@@ -0,0 +1,16 @@
+
+
+
+const NavigationBar = () => {
+    
+    return( 
+        <nav>
+        
+        <ul className="flex justify-between items-center">
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+        </ul>
+        </nav>)
+    }
+    export default NavigationBar;
