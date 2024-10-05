09881c626f6e1982f59746d8d506d25bdcd4509c
Sebastian Asprino <asprinosebastian@proton.me>
2024-10-05 16:25:50 -0500

    get y post definidi

--- backend/src/routes/model1.py
+++ backend/src/routes/model1.py
@@ -1,11 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Body
 
 router = APIRouter()
 
 @router.get("/users/")
 def read_users():
-    return [{"user_id": 1, "name": "Alice"}, {"user_id": 2, "name": "Bob"}]
+  return [{"user_id": 1, "name": "Alice"}, {"user_id": 2, "name": "Bob"}]
 
 @router.get("/users/{user_id}")
-def read_user(user_id: int):
-    return {"user_id": user_id, "name": "User {}".format(user_id)}  
+def read_user(user_id: int):  
+  return {"user_id": user_id, "name": "User {}".format(user_id)}  
+
+@router.post  ("/text/")
+def greet_user(body: dict = Body(...)):
+  name = body.get("name", "desconocido")
+  return {"message": f"Hola {name}"}
