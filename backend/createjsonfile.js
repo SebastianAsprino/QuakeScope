5394772403b3ee7d084f913bc278dd8396acff06
Sebastian Asprino <asprinosebastian@proton.me>
2024-10-08 23:52:11 -0500

    filename test push

---
+++ backend/createjsonfile.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+
+// Ruta a la carpeta 'data'
+const dataFolder = path.join(__dirname, 'data');
+
+// Lee los nombres de los archivos en la carpeta
+fs.readdir(dataFolder, (err, files) => {
+  if (err) {
+    console.error('Error al leer la carpeta:', err);
+    return;
+  }
+
+  // Crear el objeto con el formato [1: "archivo1", 2: "archivo2", ...]
+  const result = {};
+  files.forEach((file, index) => {
+    result[index + 1] = file;
+  });
+
+  // Convertir el objeto a JSON
+  const jsonResult = JSON.stringify(result, null, 2);
+
+  // Guardar el JSON resultante en un archivo
+  fs.writeFile('output.json', jsonResult, (err) => {
+    if (err) {
+      console.error('Error al escribir el archivo JSON:', err);
+      return;
+    }
+    console.log('Archivo JSON creado con éxito.');
+  });
+});
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,16 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(8000);
+const FileName: object = {
+  "1": "xa.s12.00.mhz.1969-12-16HR00_evid00006.json",
+  "2": "xa.s12.00.mhz.1970-05-20HR00_evid00026.json",
+  "3": "xa.s12.00.mhz.1970-05-23HR00_evid00027.json",
+  "4": "xa.s12.00.mhz.1970-07-27HR00_evid00039.json"
+}
+
+const app = new Elysia()
+.get("filename",() => FileName)
+
+.get("/", () => "Hello Elysia").listen(8000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
